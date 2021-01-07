696af36bce04ae552fa03a0be16e8853e4484d5c
AinazZ <ziyazova.aynaz@mail.ru>
2021-01-08 02:00:20 +0300

    showing search results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
+import ResultsList from '../components/ResultsList';
 
 import useResults from '../hooks/useResults';
 
@@ -9,11 +10,21 @@ const SearchScreen = ({ navigation }) => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults(term);
 
+    const filterResultsByPrice = (price) => {
+        // price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
     return (
         <View>
             <SearchBar term={term} onChangeTerm={setTerm} onSubmitTerm={() => searchApi(term)} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricer" />
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
         </View>
     );
 };
