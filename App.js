7519ce1b51827a3e00fea116fa7156fbb700c3f7
AinazZ <ziyazova.aynaz@mail.ru>
2021-01-07 19:23:39 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SearchScreen from './src/screens/SearchScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
